9d043e9ae88c0d640e72fa143427e771dd2c51fb
Marc DELOMEZ <marc.delomez@free.fr>
2019-03-16 11:59:55 +0100

    add resource file

---
+++ resource.h
@@ -0,0 +1,47 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by dev.rc
+//
+#define IDB_BUILD                       102
+#define IDB_RUN                         104
+#define IDI_ICON1                       105
+#define IDB_MOVEUP                      107
+#define IDB_MOVEDOWN                    108
+#define IDB_DEBUG                       110
+#define IDB_STOP                        111
+#define IDR_241                         112
+#define IDB_FIND                        113
+#define IDB_COPY_NAME                   115
+#define IDB_CLASS                       116
+#define IDB_METHOD                      117
+#define IDB_VARIABLE                    118
+#define IDB_ROOT                        119
+#define IDB_PAYPAL                      120
+#define IDB_BUTTON                      121
+#define IDB_LISTBOX                     122
+#define IDB_CHECKBOX                    123
+#define IDB_COMBOBOX                    124
+#define IDB_LABEL                       125
+#define IDB_RADIOBUTTON                 126
+#define IDB_TEXTFIELD                   127
+#define IDB_TEXTAREA                    128
+#define IDB_CALLBACK                    129
+#define IDB_OPEN                        130
+#define IDB_NEW                         131
+#define IDB_SAVE                        132
+#define IDB_CUT                         133
+#define IDB_COPY                        134
+#define IDB_PASTE                       135
+#define IDB_DELETE                      136
+#define IDB_OBJECT                      137
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
